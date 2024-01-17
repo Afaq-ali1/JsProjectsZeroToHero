eeeaee4d2dee53507c3a576d4ae40259fc83204a
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-01-17 17:10:24 +0500

    Update index.js

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,43 +1,58 @@
-function generateSystemChoice(userChoice, systemChoice) 
-{
+let score = {
+  win: 0,
+  lose: 0,
+  tie: 0,
+  displayScore: function () {
+    return `Won :${score.win}, Lost :${score.lose}, Tie :${score.tie}`
+  },
+};
+
+function generateSystemChoice(userChoice, systemChoice) {
   if (userChoice === 'rock') {
     if (systemChoice === 'Paper') {
+      score.lose++;
       return 'System won';
     }
     else if (systemChoice === 'Scissor') {
+      score.win++;
       return 'User won';
     }
     else {
+      score.tie++;
       return `It's tie`;
     }
   }
   else if (userChoice === 'paper') {
     if (systemChoice === 'Scissor') {
+      score.lose++;
       return 'System won';
     }
     else if (systemChoice === 'Rock') {
+      score.win++;
       return 'User won';
     }
     else {
+      score.tie++;
       return `It's tie`;
     }
   }
-  else
-  {
+  else {
     if (systemChoice === 'Rock') {
+      score.lose++;
       return 'System won';
     }
     else if (systemChoice === 'Paper') {
+      score.win++;
       return 'User won';
     }
     else {
+      score.tie++;
       return `It's tie`;
     }
   }
 }
 
-function randomChoice() 
-{
+function randomChoice() {
   let randomNum = Math.random() * 3;
   if (randomNum > 0 && randomNum < 1) {
     return 'Paper';
@@ -50,7 +65,11 @@ function randomChoice()
   }
 }
 
-function display(uChoice,sChoice,res)
-{
-  alert(`You choosed ${uChoice}, system choosed ${sChoice} and ${res}`);
+function display(displayUserChoice, displaySystemChoice, displayResult) {
+  alert(`You have choosen ${displayUserChoice},System choosed ${displaySystemChoice} 
+   
+   So ${displayResult}
+   
+   Results : ${score.displayScore()} 
+   `);
 }
