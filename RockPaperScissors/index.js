edb31303bae49c5ff30aff751c04cfb5be081c8a
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-01-18 23:36:20 +0500

    Update index.js

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,51 +1,52 @@
-let score = {
-  win: 0,
-  lose: 0,
-  tie: 0,
-  displayScore: function () {
-    return `Won :${score.win}, Lost :${score.lose}, Tie :${score.tie}`
-  },
-};
+let myScr = localStorage.getItem("Score");
+let score;
+resetScore(myScr);
+function resetScore(myScr) {
+  score = myScr
+    ? JSON.parse(myScr)
+    : {
+      win: 0,
+      lose: 0,
+      tie: 0,
+    };
+
+  score.displayScore = function () {
+    return ` Won :${score.win}, Lost :${score.lose}, Tie :${score.tie}`;
+  };
+  display();
+}
 
 function generateSystemChoice(userChoice, systemChoice) {
-  if (userChoice === 'rock') {
-    if (systemChoice === 'Paper') {
+  if (userChoice === "rock") {
+    if (systemChoice === "Paper") {
       score.lose++;
-      return 'System won';
-    }
-    else if (systemChoice === 'Scissor') {
+      return "System won";
+    } else if (systemChoice === "Scissor") {
       score.win++;
-      return 'User won';
-    }
-    else {
+      return "Congratulations User won !!!";
+    } else {
       score.tie++;
       return `It's tie`;
     }
-  }
-  else if (userChoice === 'paper') {
-    if (systemChoice === 'Scissor') {
+  } else if (userChoice === "paper") {
+    if (systemChoice === "Scissor") {
       score.lose++;
-      return 'System won';
-    }
-    else if (systemChoice === 'Rock') {
+      return "System won";
+    } else if (systemChoice === "Rock") {
       score.win++;
-      return 'User won';
-    }
-    else {
+      return "Congratulations User won !!!";
+    } else {
       score.tie++;
       return `It's tie`;
     }
-  }
-  else {
-    if (systemChoice === 'Rock') {
+  } else {
+    if (systemChoice === "Rock") {
       score.lose++;
-      return 'System won';
-    }
-    else if (systemChoice === 'Paper') {
+      return "System won";
+    } else if (systemChoice === "Paper") {
       score.win++;
-      return 'User won';
-    }
-    else {
+      return "Congratulations User won !!!";
+    } else {
       score.tie++;
       return `It's tie`;
     }
@@ -55,21 +56,28 @@ function generateSystemChoice(userChoice, systemChoice) {
 function randomChoice() {
   let randomNum = Math.random() * 3;
   if (randomNum > 0 && randomNum < 1) {
-    return 'Paper';
-  }
-  else if (randomNum >= 1 && randomNum < 1.8) {
-    return 'Rock';
-  }
-  else {
-    return 'Scissor';
+    return "Paper";
+  } else if (randomNum >= 1 && randomNum < 1.8) {
+    return "Rock";
+  } else {
+    return "Scissor";
   }
 }
 
 function display(displayUserChoice, displaySystemChoice, displayResult) {
-  alert(`You have choosen ${displayUserChoice},System choosed ${displaySystemChoice} 
-   
-   So ${displayResult}
-   
-   Results : ${score.displayScore()} 
-   `);
+  localStorage.setItem("Score", JSON.stringify(score));
+  document.querySelector("#userSection").innerHTML =
+    displayUserChoice
+      ? `You have choosen ${displayUserChoice}`
+      : "";
+  document.querySelector("#systemSection").innerHTML =
+    displaySystemChoice
+      ? `System choosed ${displaySystemChoice}`
+      : "";
+  document.querySelector("#resultSection").innerHTML =
+      displayResult || '';
+
+  document.querySelector("#finalSection").innerHTML = 
+      `Score :  ${score.displayScore()}`;
+
 }
