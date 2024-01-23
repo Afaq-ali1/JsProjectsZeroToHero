7aa4306f2d614ab045f51586ee9974973e39fd9d
Afaq-ali1 <aafaq.ali.5209@gmail.com>
2024-01-23 23:17:08 +0500

    js file

---
+++ JsProjectsBegtoAdv/TodoApp/todo.js
@@ -0,0 +1,32 @@
+let todoInput = document.querySelector("#todo-input");
+let todoDate = document.querySelector("#date-input");
+let todoContainer = document.querySelector("#container");
+let Arr = [
+  {
+    item: "pahla",
+    date: "2024-1-22"
+  },
+  {
+    item: "doosra",
+    date: "2024-1-22"
+  },
+];
+displayTodo();
+function displayTodo() {
+  todoContainer.innerHTML = "";
+  for (let i = 0; i < Arr.length; i++) {
+    todoContainer.innerHTML += `
+    <div>
+    <span> ${i + 1}. ${Arr[i].item} ${Arr[i].date}</span> <button   onclick="Arr.splice(${i}, 1);
+    displayTodo();">delete</button>
+    </div>
+    `;
+  }
+}
+function addTodo() {
+  let todoAdd ={
+    item: todoInput.value,
+   date: todoDate.value};
+  Arr.push(todoAdd);
+  displayTodo();
+}
